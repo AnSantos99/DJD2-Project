f25e03a8d1ff326adbd7124b99b20ed3f5032192
AnSantos99 <an.santos95@hotmail.com>
2021-12-10 11:01:43 +0000

    Remove script from prefab

--- Workorama/Assets/Scripts/Inventory/Inventory.cs
+++ Workorama/Assets/Scripts/Inventory/Inventory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory
+public class Inventory : MonoBehaviour
 {
     /// <summary>
     /// To store picked up items in a collection
