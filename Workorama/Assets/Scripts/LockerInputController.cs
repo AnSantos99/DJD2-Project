ff289dc3bcd00a5aa9c5fe0733d04fe3c822b3f9
Joana Silva <joana.f.silva.99@gmail.com>
2021-12-14 17:02:31 +0000

    Lock puzzle prefa

--- Workorama/Assets/Scripts/LockerInputController.cs
+++ Workorama/Assets/Scripts/LockerInputController.cs
@@ -20,13 +20,11 @@ public class LockerInputController : MonoBehaviour
 
     void CheckHitObj()
     {
-        Debug.Log("Clicked");
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit, reachRange))
         {
-            Debug.Log("Hit!");
             codeLock = hit.transform.gameObject.GetComponentInParent<CodeLocker>();//Witch codelock is beeing pressed
 
             if (codeLock != null)
