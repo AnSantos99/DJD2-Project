cf57fe0b6915252d54cfcf37f38bb959404aed92
AnSantos99 <an.santos95@hotmail.com>
2022-01-15 17:33:58 +0000

    Add pausemenu

--- Workorama/Assets/Scripts/MenuManagement.cs
+++ Workorama/Assets/Scripts/MenuManagement.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// Class that manages the switches between scenes
+/// Class that manages the switches between scenes and menus
 /// </summary>
 public class MenuManagement : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject pauseMenu;
+    private bool isPaused;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        Debug.Log("Am here");
+        //ActivatePauseMenu();
+    }
+
     /// <summary>
     /// Method for start button to start game
     /// </summary>
@@ -29,5 +47,38 @@ public class MenuManagement : MonoBehaviour
     {
         // To be implemented
         Debug.Log("Options menu");
+    }
+
+    /// <summary>
+    /// Method that load the main menu scene of the game
+    /// </summary>
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void ActivatePauseMenu()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            
+            PauseGame();
+        } 
+
+        else ResumeGame();      
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 }
